edb8e2eef3a26547fed251bbb5795d5206ad4972
Uday Shankar Singh <udayshankar1306@gmail.com>
2020-07-08 20:16:41 +0530

    Clean up DB Connections

--- koala/crud/user.py
+++ koala/crud/user.py
@@ -3,11 +3,12 @@ from typing import List
 
 from ..db.mongodb import AsyncIOMotorClient
 from ..models.user import UserInDB
+from ..db.mongodb import db
 
 
 async def get_comments_for_article(conn: AsyncIOMotorClient) -> List[UserInDB]:
     comments: List[UserInDB] = []
-    rows = conn["koala-backend"]["test-users"].find()
+    rows = db.client['koala-backend']['test-users'].find()
     pprint.pprint(rows)
     async for row in rows:
         comments.append(UserInDB(**row))
