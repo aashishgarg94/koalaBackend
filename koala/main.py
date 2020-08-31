696d37bf8d93288b9245e433bed1691b70949c49
Uday Shankar Singh <udayshankar1306@gmail.com>
2020-08-31 23:00:23 +0530

    added request headers for CORS

--- koala/main.py
+++ koala/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     # allow_origins=origins,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["POST", "GET", "OPTIONS", "DELETE", 'PUT'],
     allow_headers=["*"],
 )
 
