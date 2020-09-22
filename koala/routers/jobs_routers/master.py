9cbb45a6c8a93423e1f50ca23e14a82ff891ec02
Uday Shankar Singh <udayshankar1306@gmail.com>
2020-09-23 01:05:58 +0530

    Added roles in master collection

--- koala/routers/jobs_routers/master.py
+++ koala/routers/jobs_routers/master.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Security
 from koala.crud.jobs_crud.master import MasterCollections
 from koala.models.jobs_models.master import (
     GigTypeModel,
@@ -11,7 +11,11 @@ router = APIRouter()
 
 
 # Get all gigs
-@router.get("/gigs", response_model=GigTypeModel)
+@router.get(
+    "/gigs",
+    response_model=GigTypeModel,
+    dependencies=[Security(scopes=["applicant:read", "master:write"])],
+)
 async def get_gig_type():
     try:
         master_collection = MasterCollections()
@@ -22,7 +26,11 @@ async def get_gig_type():
 
 # TODO: For now getting the cities we operate in later we can get them by states
 # Get cities we operate in
-@router.get("/op_cities", response_model=OpCityModel)
+@router.get(
+    "/op_cities",
+    response_model=OpCityModel,
+    dependencies=[Security(scopes=["applicant:read", "master:write"])],
+)
 async def get_op_cities():
     try:
         master_collection = MasterCollections()
@@ -31,7 +39,11 @@ async def get_op_cities():
         raise HTTPException(status_code=500, detail="Something went wrong")
 
 
-@router.get("/op_area", response_model=OpAreaModel)
+@router.get(
+    "/op_area",
+    response_model=OpAreaModel,
+    dependencies=[Security(scopes=["applicant:read", "master:write"])],
+)
 async def get_op_cities():
     try:
         master_collection = MasterCollections()
@@ -40,7 +52,11 @@ async def get_op_cities():
         raise HTTPException(status_code=500, detail="Something went wrong")
 
 
-@router.get("/job/job_master", response_model=JobMasterModel)
+@router.get(
+    "/job/job_master",
+    response_model=JobMasterModel,
+    dependencies=[Security(scopes=["applicant:read", "master:write"])],
+)
 async def get_job_master():
     try:
         master_collection = MasterCollections()
