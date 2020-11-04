f8d6dc7732c9783be37525c64c21dffd96df1409
Uday Shankar Singh <udayshankar1306@gmail.com>
2020-11-04 14:26:57 +0530

    Added production database

--- koala/db/mongo_adaptor.py
+++ koala/db/mongo_adaptor.py
@@ -4,12 +4,12 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from .mongodb import db
 
-
+# mongodb+srv://uday:koala1mongo@cluster0.ca4yg.mongodb.net/production?retryWrites=true&w=majority
 async def connect_to_mongo():
     logging.info("Connecting to mongo...")
     db.client = AsyncIOMotorClient(
         str(
-            "mongodb+srv://uday:koala1mongo@cluster0.tbo7y.mongodb.net/koala-backend?retryWrites=true&w=majority"
+            "mongodb+srv://uday:koala1mongo@cluster0.ca4yg.mongodb.net/production?retryWrites=true&w=majority"
         ),
         maxPoolSize=10,
         minPoolSize=10,
