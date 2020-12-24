1d3da636d62dffd6ae943e6847de247aa6e14fab
Uday Shankar Singh <udayshankar.singh1@tesco.com>
2020-12-24 11:57:02 +0530

    Added supported version api and moved group image to base instead of IN

--- koala/routers/supported_version.py
+++ koala/routers/supported_version.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter, HTTPException
+import logging
+
+from fastapi import APIRouter
 
 router = APIRouter()
 
 
-@router.get(
+@router.post(
     "/supported_version",
 )
-async def generate_otp():
-    return ['1.0.0', '1.2.0']
+async def supported_version(app_version: str, build_version: str):
+    logging.info(app_version)
+    logging.info(build_version)
+    data = ['1.0.0', '1.2.0']
+    return False
