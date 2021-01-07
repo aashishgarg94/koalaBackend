f99a5d3029ac72f0df184a6611600cc331a0d678
Uday Shankar Singh <udayshankar.singh1@tesco.com>
2021-01-07 17:24:00 +0530

    Telesign integration complete, switched messaging urls

--- koala/modules/auth/utils/otp.py
+++ koala/modules/auth/utils/otp.py
@@ -1,5 +1,7 @@
+import json
 import logging
 
+import requests
 from telesign.util import random_with_n_digits
 from telesign.messaging import MessagingClient
 
@@ -8,6 +10,9 @@ from koala.modules.auth.utils.constants import (
     TELESIGN_CUSTOMER_ID,
     TELESIGN_API_KEY,
     TELESIGN_MESSAGE_TYPE,
+    MSG91_URL,
+    MSG91_AUTHKEY,
+    MSG91_TEMPLATE_ID,
 )
 
 
@@ -27,5 +32,20 @@ async def telesign_send_otp(phone_number: str, verify_code: int) -> any:
         messaging = MessagingClient(TELESIGN_CUSTOMER_ID, TELESIGN_API_KEY)
         return messaging.message(phone_number, message, TELESIGN_MESSAGE_TYPE)
     except Exception as e:
-        logging.error(f"Error: while generating OTP {e}")
+        logging.error(f"Error: while sending OTP {e}")
+        raise e
+
+
+async def msg91_resend_otp(phone_number: str, verify_code: int) -> any:
+    try:
+        url = f"{MSG91_URL}?authkey={MSG91_AUTHKEY}&template_id={MSG91_TEMPLATE_ID}&mobile=+91{phone_number}&otp={verify_code}"
+
+        payload = {}
+        headers = {"content-type": "application/json"}
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return json.loads(response.text)
+
+    except Exception as e:
+        logging.error(f"Error: while re-sending OTP {e}")
         raise e
