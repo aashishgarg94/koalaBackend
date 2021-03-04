7bab1cd07839f43ed6f45d1082d6d51019feb7a0
Uday Shankar Singh <udayshankar.singh1@tesco.com>
2021-03-04 21:57:56 +0530

    noti check in progress

--- koala/modules/notifications/curd/notification.py
+++ koala/modules/notifications/curd/notification.py
@@ -1,33 +1,20 @@
 import logging
-import requests
-import json
+from pyfcm import FCMNotification
+
+API_KEY = "AIzaSyAWSdnv5O8Ai49W0FdFwYVMrq4bl7-_VvM"
+
 
 class Notifications:
     @staticmethod
     async def send_notifications(fcm_tokens: list) -> any:
         try:
-            for fcm_token in fcm_tokens:
-                server_token = 'your server key here'
-                device_token = 'device token here'
+            push_service = FCMNotification(api_key=API_KEY)
+            registration_ids = ["<device registration_id 1>", "<device registration_id 2>", ...]
+            message_title = "Pragaty notification"
+            message_body = "Long time no notification from pragaty"
+            result = push_service.notify_multiple_devices(registration_ids=registration_ids,
+                                                          message_title=message_title, message_body=message_body)
 
-                headers = {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'key=' + server_token,
-                }
-
-                body = {
-                    'notification': {'title': 'Sending push form python script',
-                                     'body': 'New Message'
-                                     },
-                    'to':
-                        device_token,
-                    'priority': 'high',
-                    #   'data': dataPayLoad,
-                }
-                response = requests.post("https://fcm.googleapis.com/fcm/send", headers=headers, data=json.dumps(body))
-                print(response.status_code)
-
-                print(response.json())
-                pass
+            print(result)
         except Exception as e:
             logging.error(f"Error: while saving device details - {e}")
